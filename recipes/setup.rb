36f9dac7a215aeeaf054e7bae5acc3685dbc1552
Joel Mann <jmann.creston@gmail.com>
2022-02-03 18:20:52 +0000

    User and group functions

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -32,6 +32,17 @@ template '/etc/motd' do
     action :create
 end
 
+user 'user1' do
+    comment 'user1'
+    uid '123'
+    home '/home/user1'
+    shell '/bin/bash'
+end
+
+group 'admins' do
+    members ['user1','vagrant']
+    append true
+end
 
 # Assignment:
 # Chef install 2 packages - Tree, NTP
